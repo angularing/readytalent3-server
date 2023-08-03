bdd843b9588ff7926b6f132790136e734fb6a527
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-03 20:14:41 +0800

    initial

---
+++ src/test/java/com/angularing/readytalent3/Readytalent3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularing.readytalent3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Readytalent3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
