1a7757b0535a35164f512608a9dec269a4bc02b0
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-20 11:57:21 +0800

    CORS

--- src/main/java/com/angularing/readytalent3/JobRepository.java
+++ src/main/java/com/angularing/readytalent3/JobRepository.java
@@ -2,9 +2,11 @@ package com.angularing.readytalent3;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200", "https://readytalent3.vercel.app/"})
 @RepositoryRestResource(collectionResourceRel = "jobs", path = "jobs")
 public interface JobRepository extends CrudRepository<Job, Long> {
     List<Job> findByTitle(String title);
